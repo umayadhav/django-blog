0e5aece08774f539d31d307891335fc6c8700331
uma yadhav <uma389142021@gmail.com>
2026-01-17 13:50:45 +0530

    post by category and template inheritance

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from blogs .models import Category,Blog
+
 
 def home(request):
-    return render(request,'home.html')
+  
+    featured_posts=Blog.objects.filter(is_featured=True).order_by('-updated_at')
+    posts=Blog.objects.filter(is_featured=False)
+    context={
+        
+        'featured_posts':featured_posts,
+        'posts':posts,
+    }
+    return render(request,'home.html',context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
 
-# Create your views here.
+
+def posts_by_category(request,category_id):
+    #fetch the post that belongs to the category with id category_id
+    posts = Blog.objects.filter(status="published",category=category_id)
+    #use try /except when we want to do some custom action if the category does not exists
+    '''try:
+
+        category= Category.objects.get(pk =category_id)
+    except:
+        #redirect the user to the home page
+        return redirect('home') 
+        #use get_object_or_404 when you want to show 404 error page if the category does not exits'''
+    category= get_object_or_404(Category,pk=category_id)   
+    context={
+        'posts':posts,
+        'category':category,
+    }
+    return render(request,"posts_by_category.html",context)
